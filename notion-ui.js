b737c3913e175b1807bfdf0da65676d616b27989
letsusethisid <43811006+letsusethisid@users.noreply.github.com>
2022-11-17 12:09:57 +0530

    add notion ui js file

---
+++ notion-ui.js
@@ -0,0 +1,35 @@
+const afterBegin = 'afterbegin';
+
+const setUI = () => {
+   const isMobileView = window.innerWidth <= 768;
+   console.log('isMobileView -> ', isMobileView);
+   !isMobileView && loadTableOfContentMenu();
+}
+
+const loadTableOfContentMenu = () => {
+  const tableOfContent = document.querySelector('.notion-table_of_contents-block');
+  const tableOfContentHTML = tableOfContent.outerHTML;
+  console.log('tableOfContent -> ', tableOfContent);
+  const notionFrameDiv = document.querySelector('.notion-frame');
+  console.log('notionFrameDiv -> ', notionFrameDiv);
+  console.log('tableOfContentHTML -> ', tableOfContentHTML);
+  notionFrameDiv.insertAdjacentHTML(afterBegin, tableOfContentHTML);
+  tableOfContent.remove();
+  removeChildren(tableOfContent);
+}
+
+const removeChildren = (element) => {
+  [...element.children].map((child) => child.remove());
+}
+
+
+const removeChildrenExceptOne = (element, childClassName) => {
+  [...element.children].map((child) => {
+    if (!child.classList.contains(childClassName)) {
+      child.remove();
+    }
+  });
+}
+
+
+setUI();
